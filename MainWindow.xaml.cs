942ed0abad3b277b9bbd83f9f457295cabbd1353
301k10 <301k10@WIN-9M04LT12L1F>
2024-02-26 20:02:20 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            getPokemon();
+        }
+
+        public async void getPokemon()
+        {
+            using (var http = new HttpClient()) {
+                try {
+                    var resp = await http.GetStringAsync("https://pokeapi.co/api/v2/pokemon/"+ pokeName.Text.ToLower());
+                    dynamic? result = JsonConvert.DeserializeObject(resp);
+                    string name = result["name"];
+                    int weight = result["weight"];
+                    string urlImage = result["sprites"]["front_default"];
+                    string backImage = result["sprites"]["back_default"];
+                    pokeFrame.Content = new Pokemon(name,urlImage,weight, backImage);
+                }catch(HttpRequestException exp)
+                {
+                    MessageBox.Show(exp.Message);
+                }
+            }
+        }
+
+
+        
+
+        
+    
+    }
+}
---
+++ Pokemon.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для Pokemon.xaml
+    /// </summary>
+    public partial class Pokemon : Page
+    {
+        public Pokemon(string name, string urlImage, int weight, string backImage)
+        {
+            InitializeComponent();
+
+            pokeName.Text = name;
+            pokeWeight.Text = weight.ToString();
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(urlImage);
+            bitmap.EndInit();
+
+            BitmapImage bitmap1 = new BitmapImage();
+            bitmap1.BeginInit();
+            bitmap1.UriSource = new Uri(backImage);
+            bitmap1.EndInit();
+
+
+            frontPIc.Source = bitmap;
+            backPic.Source = bitmap1;
+        }
+    }
+}
